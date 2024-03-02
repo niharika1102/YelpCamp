812f27764aab7774203b685f3a4d51d0db0fc83b
niharika1102 <niharikag1102@gmail.com>
2024-03-02 08:48:37 +0530

    Error Template
    Setup a basic error template (shows error stack)

--- app.js
+++ app.js
@@ -89,8 +89,11 @@ app.all('*', (req, res, next) => {
 
 //Custom error handler
 app.use((err, req, res, next) => {
-    const {statusCode = 500, message = 'Something went wrong'} = err;
-    res.status(statusCode).send(message);
+    const {statusCode = 500} = err;
+    if (!err.message) {
+        err.message = 'Something went wrong!!';
+    }
+    res.status(statusCode).render('error', {err});
 })
 
 app.listen(3000, () => {
