21513289e954db2b0255ce1816cc3dda6f9796b9
niharika1102 <niharikag1102@gmail.com>
2024-04-11 22:50:01 +0530

    Delete middleware
    Deleting all reviews to a campground if the campground is removed.

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;       //used as a shorthand for mongoose.Schema
+const Review = require("./review")
 
 const CampgroundSchema = new Schema ({
     name: {
@@ -28,6 +29,18 @@ const CampgroundSchema = new Schema ({
             ref: "Review"
         }
     ]
+})
+
+CampgroundSchema.post('findOneAndDelete',async function(camp){
+    if(camp){
+        await Review.deleteMany(
+            {
+                _id: {
+                    $in: camp.reviews
+                }
+            }
+        )
+    }
 })
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
