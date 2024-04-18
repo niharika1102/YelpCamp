ed1514476958d48aa702fc8a26b9c9fdae99b334
niharika1102 <niharikag1102@gmail.com>
2024-04-18 21:43:51 +0530

    Register logic
    Logic to register and error while registering duplicate user added

--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,28 @@ const router = express.Router();
 //Schema calling
 const User = require('../models/user');
 
+//Utils call
+const catchAsync = require('../utils/CatchAsync');
+const ExpressError = require('../utils/ExpressError');
+
 //Get the registeration form
 router.get('/register', (req, res) => {
     res.render('users/register')
 })
 
 //Saving userdata
-router.post('/register', async(req, res) => {
-    res.send(req.body);
-})
+router.post('/register', catchAsync(async(req, res) => {
+    try {
+        const {email, username, password} = req.body;
+        const user = new User({email, username});
+        const registeredUser = await User.register(user, password);
+        req.flash('success', "Welcome to YelpCamp!");
+        res.redirect('/campgrounds');
+    }
+    catch (e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+}));
 
 module.exports = router;
