a9a757bbf0cb553308bfc7234260e9d0eee66076
niharika1102 <niharikag1102@gmail.com>
2024-04-23 01:47:53 +0530

    Map Style
    Changed map style

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
+    style: 'mapbox://styles/mapbox/outdoors-v9', // stylesheet location
     center: camp.geometry.coordinates, // [lng, lat] coordinates of the campground
     zoom: 10 // starting zoom
 });
@@ -12,6 +12,6 @@ new mapboxgl.Marker()
     .setLngLat(camp.geometry.coordinates) // Set marker location
     .setPopup(
         new mapboxgl.Popup({offset: 25})
-            .setHTML(`<h3>${camp.name}</h3><p>${camp.location}</p>`) // Update to use campground name and location
+            .setHTML(`<h5>${camp.name}</h5><p>${camp.location}</p>`) // Update to use campground name and location
     )
     .addTo(map);
